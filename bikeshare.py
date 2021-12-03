68e0a6eaba160d6fa3d657cc5ba8dfc952ae8660
SheebaGH <94782975+SheebaGH@users.noreply.github.com>
2021-12-04 00:56:30 +0200

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -38,19 +38,16 @@ def get_from_files(city, month, day):
     df = pd.read_csv(raw[city])
     
 def load_data(city, month, day):
-    df = pd.read_csv(CITY_DATA[city])
-
-    df['Start Time'] = pd.to_datetime(df['Start Time'])
-	
-    df['month'] = df['Start Time'].dt.month
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
-    df['hour'] = df['Start Time'].dt.hour
-    if month != 'all':
-    	months = ['january','february','march','april','may','june']
-    	month = months.index(month) + 1
-	df = df[df['month'] == month]
-    if day != 'all':
-	df = df[df['day_of_week'] == day.title()]
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+    
+     Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     return df
     
